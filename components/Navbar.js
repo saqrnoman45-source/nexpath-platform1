77d84d29c0715c2edabe39d6ad53b6223ab316bb
saqrnoman45-source <saqrnoman45@gmail.com>
2026-05-14 19:52:00 +0300

    Add Navbar component with admin controls

---
+++ components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="bg-white p-4 shadow flex justify-between">
+      <span>لوحة التحكم</span>
+      <span>👤 Admin</span>
+    </div>
+  );
+}
